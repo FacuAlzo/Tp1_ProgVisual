f6f960d26df2f425159a44643937bb5d91fd89f2
FacuAlzo <facux.desertor@gmail.com>
2024-04-04 17:22:01 -0300

    Ejercicio calculo de promedio

---
+++ tp1_AlzogarayFacundo/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nota1 = 8;
+	    int nota2 = 6;
+	    int nota3 = 5;
+	    int nota4 = 7;
+	    int nota5 = 5;
+	    int promedio = (nota1 + nota2 + nota3 + nota4 + nota5)/ 5;
+	    System.out.println("Nota1: "+nota1);
+	    System.out.println("Nota2: "+nota2);
+	    System.out.println("Nota3: "+nota3);
+	    System.out.println("Nota4: "+nota4);
+	    System.out.println("Nota5: "+nota5);
+	    System.out.println("El promedio es: "+promedio);
+	}
+
+}
